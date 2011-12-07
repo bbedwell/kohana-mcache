534110a4ce078baafef1206ecec36b677942dcae
Bryce Bedwell <brycebedwell@gmail.com>
2011-12-07 13:12:24 -0700

    Added: check if force_execute is false on getting cache

--- classes/database/query.php
+++ classes/database/query.php
@@ -41,7 +41,7 @@ class Database_Query extends Kohana_Database_Query {
 		// Compile the SQL query
 		$sql = $this->compile($db);
 		
-		if ($this->_lifetime !== NULL AND $this->_type === Database::SELECT AND $cache_available)
+		if ($this->_lifetime !== NULL AND !$this->_force_execute AND $this->_type === Database::SELECT AND $cache_available)
 		{
 			if( ($result = $mcache->get($sql)) !== NULL AND !$this->_force_execute)
 			{
